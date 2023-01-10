fbd54fea9c0b611c40acc2dcd56a861bdebed81a
Afrah2k <117500357+ThajulAfrah21@users.noreply.github.com>
2023-01-10 11:19:04 +0530

    Add files via upload

---
+++ login/login.js
@@ -0,0 +1,24 @@
+function myfunction() {
+    var x = document.getElementById("pass");
+
+    if (x.type === "password") {
+        x.type = "text";
+    }
+    else {
+        x.type = "password";
+    }
+}
+
+function validate() {
+    var password = document.getElementById("pass");
+    var length = document.getElementById("length");
+
+    if (password.value.length >= 8) {
+        alert("Login Succesfull");
+        window.location.replace("doctor.html");
+        return false;
+    }
+    else {
+        alert("Login Failed");
+    }
+}
