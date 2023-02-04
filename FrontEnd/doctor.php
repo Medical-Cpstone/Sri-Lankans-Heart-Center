c704f1085bbca776644e9441806211889eaca3e5
LAPTOP-2K448739\Ishka Insaaf <ishkainsaf28@gmail.com>
2023-02-04 17:18:56 +0530

    updated doctor.php

--- FrontEnd/doctor.php
+++ FrontEnd/doctor.php
@@ -6,7 +6,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" />
   <link rel="stylesheet" href="/CSS/doctor.css">
-  <title>Our project</title>
+  <title>meet your doctor</title>
 </head>
 
 <body>
