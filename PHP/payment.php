358c66445cb1d6fb97ee6395f9277939826e2d1b
LAPTOP-2K448739\Ishka Insaaf <ishkainsaf28@gmail.com>
2023-02-08 18:27:58 +0530

    added changes

--- PHP/payment.php
+++ PHP/payment.php
@@ -2,21 +2,14 @@
 
 $conn = mysqli_connect('localhost','root','','hospital') or die('connection failed');
 
-$Full_name = mysqli_real_escape_string($conn, $_POST['fname']);
-$Email = mysqli_real_escape_string($conn, $_POST['email']);
-$Address = $_POST['adress'];
-$City = $_POST['city'];
-$State =$_POST['stat'];
-$Zip_code =$_POST['zip_code'];
-$Name_on_card =$_POST['name_on_card'];
-$Credit_card_number =$_POST['credit_card_number'];
-$Exp_month =$_POST['exp_month'];
-$Exp_year =$_POST['exp_year'];
-$cvv =$_POST['cvv'];
+$card_holder=$_POST['card_holder'];
+$card_number =$_POST['credit_card_number'];
+$exp_date =$_POST['exp_date'];
+$cvc =$_POST['cvc'];
  
    
    if(isset($_POST['submit'])){
-$insert = mysqli_query($conn, "INSERT INTO `payment_table`(fname, email, adress, city, stat, zip_code, name_on_card, credit_card_number, exp_month, exp_year, cvv) VALUES('$Full_name','$Email','$Address','$City','$State','$Zip_code','$Name_on_card','$Credit_card_number','$Exp_month','$Exp_year','$cvv')") or die('query failed');
+$insert = mysqli_query($conn, "INSERT INTO `payment_table`(card_holder, card_number, exp_date, cvc) VALUES('card_holder','$card_number','$exp_date','$cvc')") or die('query failed');
  
 
    if($insert){
