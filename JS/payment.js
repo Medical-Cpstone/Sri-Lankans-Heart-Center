8e8cd1544fb77d8ed45af385674e0a44c51e3201
LAPTOP-2K448739\Ishka Insaaf <ishkainsaf28@gmail.com>
2023-02-09 01:02:48 +0530

    added changes

--- JS/payment.js
+++ JS/payment.js
@@ -1,1 +1,24 @@
 $("input[name='expiry-data']").mask("00 / 00");
+
+function myFunction() {
+    var x = document.getElementById("pass");
+
+    if (x.type === "text") {
+        x.type = "text";
+    } else {
+        x.type = "text";
+    }
+}
+
+function validate() {
+    var text = document.getElementById("pass");
+    var length = document.getElementById("length");
+
+    if (text.value.length = 16) {
+        alert("Payment Successful");
+        window.location.replace("index.html");
+        return false;
+    } else {
+        alert("payment Failed");
+    }
+}
