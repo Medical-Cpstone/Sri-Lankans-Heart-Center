972c9b878d56b5a06eb206d54a0550f45bbb95ca
Ishka Insaaf <106860842+ishka28@users.noreply.github.com>
2023-03-05 22:43:10 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,1 +1,8 @@
+<?php
 
+    $database= new mysqli("localhost","root","","HeartCenter");
+    if ($database->connect_error){
+        die("Connection failed:  ".$database->connect_error);
+    }
+
+?>
