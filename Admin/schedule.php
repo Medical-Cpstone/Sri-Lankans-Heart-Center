33a2e457ef5fefed098bf30d797c61fa108ee1ae
LAPTOP-2K448739\Ishka Insaaf <ishkainsaf28@gmail.com>
2023-03-21 19:11:29 +0530

    Updated schedule.php

--- Admin/schedule.php
+++ Admin/schedule.php
@@ -1,25 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/animations.css">  
-    <link rel="stylesheet" href="../css/main.css">  
-    <link rel="stylesheet" href="../css/admin.css">
-        
-    <title>Schedule</title>
-    <style>
-        .popup{
-            animation: transitionIn-Y-bottom 0.5s;
-        }
-        .sub-table{
-            animation: transitionIn-Y-bottom 0.5s;
-        }
-</style>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../css/animations.css">
+  <link rel="stylesheet" href="../css/main.css">
+  <link rel="stylesheet" href="../css/admin.css">
+
+  <title>Schedule</title>
+  <style>
+  .popup {
+    animation: transitionIn-Y-bottom 0.5s;
+  }
+
+  .sub-table {
+    animation: transitionIn-Y-bottom 0.5s;
+  }
+  </style>
 </head>
+
 <body>
-    <?php
+  <?php
 
     session_start();
 
@@ -31,84 +34,104 @@
     }else{
         header("location: ../login.php");
     }
-    
-    
 
     //import database
     include("../connection.php");
 
     
     ?>
-    <div class="container">
-        <div class="menu">
-            <table class="menu-container" border="0">
-                <tr>
-                    <td style="padding:10px" colspan="2">
-                        <table border="0" class="profile-container">
-                            <tr>
-                                <td width="30%" style="padding-left:20px" >
-                                    <img src="../img/user.png" alt="" width="100%" style="border-radius:50%">
-                                </td>
-                                <td style="padding:0px;margin:0px;">
-                                    <p class="profile-title">Administrator</p>
-                                    <p class="profile-subtitle">admin@edoc.com</p>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td colspan="2">
-                                <a href="../logout.php" ><input type="button" value="Log out" class="logout-btn btn-primary-soft btn"></a>
-                                </td>
-                            </tr>
-                    </table>
-                    </td>
-                
-                </tr>
-                <tr class="menu-row" >
-                    <td class="menu-btn menu-icon-dashbord" >
-                        <a href="index.php" class="non-style-link-menu"><div><p class="menu-text">Dashboard</p></a></div></a>
-                    </td>
-                </tr>
-                <tr class="menu-row">
-                    <td class="menu-btn menu-icon-doctor ">
-                        <a href="doctors.php" class="non-style-link-menu "><div><p class="menu-text">Doctors</p></a></div>
-                    </td>
-                </tr>
-                <tr class="menu-row" >
-                    <td class="menu-btn menu-icon-schedule menu-active menu-icon-schedule-active">
-                        <a href="schedule.php" class="non-style-link-menu non-style-link-menu-active"><div><p class="menu-text">Schedule</p></div></a>
-                    </td>
-                </tr>
-                <tr class="menu-row">
-                    <td class="menu-btn menu-icon-appoinment">
-                        <a href="appointment.php" class="non-style-link-menu"><div><p class="menu-text">Appointment</p></a></div>
-                    </td>
-                </tr>
-                <tr class="menu-row" >
-                    <td class="menu-btn menu-icon-patient">
-                        <a href="patient.php" class="non-style-link-menu"><div><p class="menu-text">Patients</p></a></div>
-                    </td>
-                </tr>
-
+  <div class="container">
+    <div class="menu">
+      <table class="menu-container" border="0">
+        <tr>
+          <td style="padding:10px" colspan="2">
+            <table border="0" class="profile-container">
+              <tr>
+                <td width="30%" style="padding-left:20px">
+                  <img src="../img/user.png" alt="" width="100%" style="border-radius:50%">
+                </td>
+                <td style="padding:0px;margin:0px;">
+                  <p class="profile-title">Administrator</p>
+                  <p class="profile-subtitle">admin@edoc.com</p>
+                </td>
+              </tr>
+              <tr>
+                <td colspan="2">
+                  <a href="../logout.php"><input type="button" value="Log out"
+                      class="logout-btn btn-primary-soft btn"></a>
+                </td>
+              </tr>
             </table>
-        </div>
-        <div class="dash-body">
-            <table border="0" width="100%" style=" border-spacing: 0;margin:0;padding:0;margin-top:25px; ">
-                <tr >
-                    <td width="13%" >
-                    <a href="schedule.php" ><button  class="login-btn btn-primary-soft btn btn-icon-back"  style="padding-top:11px;padding-bottom:11px;margin-left:20px;width:125px"><font class="tn-in-text">Back</font></button></a>
-                    </td>
-                    <td>
-                        <p style="font-size: 23px;padding-left:12px;font-weight: 600;">Shedule Manager</p>
-                                           
-                    </td>
-                    <td width="15%">
-                        <p style="font-size: 14px;color: rgb(119, 119, 119);padding: 0;margin: 0;text-align: right;">
-                            Today's Date
-                        </p>
-                        <p class="heading-sub12" style="padding: 0;margin: 0;">
-                            <?php 
+          </td>
 
-                        date_default_timezone_set('Asia/Kolkata');
+        </tr>
+        <tr class="menu-row">
+          <td class="menu-btn menu-icon-dashbord">
+            <a href="index.php" class="non-style-link-menu">
+              <div>
+                <p class="menu-text">Dashboard</p>
+            </a>
+    </div></a>
+    </td>
+    </tr>
+    <tr class="menu-row">
+      <td class="menu-btn menu-icon-doctor ">
+        <a href="doctors.php" class="non-style-link-menu ">
+          <div>
+            <p class="menu-text">Doctors</p>
+        </a>
+  </div>
+  </td>
+  </tr>
+  <tr class="menu-row">
+    <td class="menu-btn menu-icon-schedule menu-active menu-icon-schedule-active">
+      <a href="schedule.php" class="non-style-link-menu non-style-link-menu-active">
+        <div>
+          <p class="menu-text">Schedule</p>
+        </div>
+      </a>
+    </td>
+  </tr>
+  <tr class="menu-row">
+    <td class="menu-btn menu-icon-appoinment">
+      <a href="appointment.php" class="non-style-link-menu">
+        <div>
+          <p class="menu-text">Appointment</p>
+      </a></div>
+    </td>
+  </tr>
+  <tr class="menu-row">
+    <td class="menu-btn menu-icon-patient">
+      <a href="patient.php" class="non-style-link-menu">
+        <div>
+          <p class="menu-text">Patients</p>
+      </a></div>
+    </td>
+  </tr>
+
+  </table>
+  </div>
+  <div class="dash-body">
+    <table border="0" width="100%" style=" border-spacing: 0;margin:0;padding:0;margin-top:25px; ">
+      <tr>
+        <td width="13%">
+          <a href="schedule.php"><button class="login-btn btn-primary-soft btn btn-icon-back"
+              style="padding-top:11px;padding-bottom:11px;margin-left:20px;width:125px">
+              <font class="tn-in-text">Back</font>
+            </button></a>
+        </td>
+        <td>
+          <p style="font-size: 23px;padding-left:12px;font-weight: 600;">Schedule Manager</p>
+
+        </td>
+        <td width="15%">
+          <p style="font-size: 14px;color: rgb(119, 119, 119);padding: 0;margin: 0;text-align: right;">
+            Today's Date
+          </p>
+          <p class="heading-sub12" style="padding: 0;margin: 0;">
+            <?php 
+
+                        date_default_timezone_set('Asia/Colombo');
 
                         $today = date('Y-m-d');
                         echo $today;
@@ -116,56 +139,51 @@
                         $list110 = $database->query("select  * from  schedule;");
 
                         ?>
-                        </p>
-                    </td>
-                    <td width="10%">
-                        <button  class="btn-label"  style="display: flex;justify-content: center;align-items: center;"><img src="../img/calendar.svg" width="100%"></button>
-                    </td>
+          </p>
+        </td>
+        <td width="10%">
+          <button class="btn-label" style="display: flex;justify-content: center;align-items: center;"><img
+              src="../img/calendar.svg" width="100%"></button>
+        </td>
 
 
-                </tr>
-               
-                <tr>
-                    <td colspan="4" >
-                        <div style="display: flex;margin-top: 40px;">
-                        <div class="heading-main12" style="margin-left: 45px;font-size:20px;color:rgb(49, 49, 49);margin-top: 5px;">Schedule a Session</div>
-                        <a href="?action=add-session&id=none&error=0" class="non-style-link"><button  class="login-btn btn-primary btn button-icon"  style="margin-left:25px;background-image: url('../img/icons/add.svg');">Add a Session</font></button>
-                        </a>
-                        </div>
-                    </td>
-                </tr>
-                <tr>
-                    <td colspan="4" style="padding-top:10px;width: 100%;" >
-                    
-                        <p class="heading-main12" style="margin-left: 45px;font-size:18px;color:rgb(49, 49, 49)">All Sessions (<?php echo $list110->num_rows; ?>)</p>
-                    </td>
-                    
-                </tr>
-                <tr>
-                    <td colspan="4" style="padding-top:0px;width: 100%;" >
-                        <center>
-                        <table class="filter-container" border="0" >
-                        <tr>
-                           <td width="10%">
+      </tr>
 
-                           </td> 
-                        <td width="5%" style="text-align: center;">
-                        Date:
-                        </td>
-                        <td width="30%">
-                        <form action="" method="post">
-                            
-                            <input type="date" name="sheduledate" id="date" class="input-text filter-container-items" style="margin: 0;width: 95%;">
+      <tr>
+        <td colspan="4">
+          <div style="display: flex;margin-top: 40px;">
+            <div class="heading-main12" style="margin-left: 45px;font-size:20px;color:rgb(49, 49, 49);margin-top: 5px;">
+              Schedule a Session</div>
+            <a href="?action=add-session&id=none&error=0" class="non-style-link"><button
+                class="login-btn btn-primary btn button-icon"
+                style="margin-left:25px;background-image: url('../img/icons/add.svg');">Add a Session</font></button>
+            </a>
+          </div>
+        </td>
+      </tr>
+      <tr>
+        <td colspan="4" style="padding-top:10px;width: 100%;">
 
-                        </td>
-                        <td width="5%" style="text-align: center;">
-                        Doctor:
-                        </td>
-                        <td width="30%">
-                        <select name="docid" id="" class="box filter-container-items" style="width:90% ;height: 37px;margin: 0;" >
-                            <option value="" disabled selected hidden>Choose Doctor Name from the list</option><br/>
-                                
-                            <?php 
+          <p class="heading-main12" style="margin-left: 45px;font-size:18px;color:rgb(49, 49, 49)">All Sessions
+            (<?php echo $list110->num_rows; ?>)</p>
+        </td>
+      </tr>
+      <tr>
+        <td colspan="4" style="padding-top:0px;width: 100%;">
+          <center>
+            <table class="filter-container" border="0">
+              <tr>
+                <td width="10%">
+                </td>
+                <td width="5%" style="text-align: center;">
+                  Doctor:
+                </td>
+                <td width="30%">
+                  <select name="docid" id="" class="box filter-container-items"
+                    style="width:90% ;height: 37px;margin: 0;">
+                    <option value="" disabled selected hidden>Choose Doctor Name from the list</option><br />
+
+                    <?php 
                             
                                 $list11 = $database->query("select  * from  doctor order by docname asc;");
 
@@ -176,25 +194,25 @@
                                     echo "<option value=".$id00.">$sn</option><br/>";
                                 };
 
-
                                 ?>
 
-                        </select>
-                    </td>
-                    <td width="12%">
-                        <input type="submit"  name="filter" value=" Filter" class=" btn-primary-soft btn button-icon btn-filter"  style="padding: 15px; margin :0;width:100%">
-                        </form>
-                    </td>
+                  </select>
+                </td>
+                <td width="12%">
+                  <input type="submit" name="filter" value=" Filter"
+                    class=" btn-primary-soft btn button-icon btn-filter" style="padding: 15px; margin :0;width:100%">
+                  </form>
+                </td>
 
-                    </tr>
-                            </table>
+              </tr>
+            </table>
 
-                        </center>
-                    </td>
-                    
-                </tr>
-                
-                <?php
+          </center>
+        </td>
+
+      </tr>
+
+      <?php
                     if($_POST){
                         //print_r($_POST);
                         $sqlpt1="";
@@ -235,44 +253,44 @@
 
 
                 ?>
-                  
-                <tr>
-                   <td colspan="4">
-                       <center>
-                        <div class="abc scroll">
-                        <table width="93%" class="sub-table scrolldown" border="0">
-                        <thead>
-                        <tr>
-                                <th class="table-headin">
-                                    
-                                
-                                Session Title
-                                
-                                </th>
-                                
-                                <th class="table-headin">
-                                    Doctor
-                                </th>
-                                <th class="table-headin">
-                                    
-                                    Sheduled Date & Time
-                                    
-                                </th>
-                                <th class="table-headin">
-                                    
-                                Max num that can be booked
-                                    
-                                </th>
-                                
-                                <th class="table-headin">
-                                    
-                                    Events
-                                    
-                                </tr>
-                        </thead>
-                        <tbody>
-                        
-                            <?php
+
+      <tr>
+        <td colspan="4">
+          <center>
+            <div class="abc scroll">
+              <table width="93%" class="sub-table scrolldown" border="0">
+                <thead>
+                  <tr>
+                    <th class="table-headin">
+
+
+                      Session Title
+
+                    </th>
+
+                    <th class="table-headin">
+                      Doctor
+                    </th>
+                    <th class="table-headin">
+
+                      Sheduled Date & Time
+
+                    </th>
+                    <th class="table-headin">
+
+                      Max num that can be booked
+
+                    </th>
+
+                    <th class="table-headin">
+
+                      Events
+
+                  </tr>
+                </thead>
+                <tbody>
+
+                  <?php
 
                                 
                                 $result= $database->query($sqlmain);
@@ -331,21 +349,21 @@
                             }
                                  
                             ?>
- 
-                            </tbody>
 
-                        </table>
-                        </div>
-                        </center>
-                   </td> 
-                </tr>
-                       
-                        
-                        
-            </table>
-        </div>
-    </div>
-    <?php
+                </tbody>
+
+              </table>
+            </div>
+          </center>
+        </td>
+      </tr>
+
+
+
+    </table>
+  </div>
+  </div>
+  <?php
     
     if($_GET){
         $id=$_GET["id"];
@@ -692,7 +710,8 @@
 }
         
     ?>
-    </div>
+  </div>
 
 </body>
+
 </html>
