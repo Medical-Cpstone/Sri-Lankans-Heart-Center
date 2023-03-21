48626c18e907a4b79f2c0aba78fb3101adfef396
LAPTOP-2K448739\Ishka Insaaf <ishkainsaf28@gmail.com>
2023-03-21 10:40:09 +0530

    Updated setting.php

--- patient/settings.php
+++ patient/settings.php
@@ -1,35 +1,35 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/animations.css">  
-    <link rel="stylesheet" href="../css/main.css">  
-    <link rel="stylesheet" href="../css/admin.css">
-        
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../css/animations.css">
+  <link rel="stylesheet" href="../css/main.css">
+  <link rel="stylesheet" href="../css/admin.css">
 
 
-    <title>Settings</title>
-    <style>
-        .dashbord-tables{
-            animation: transitionIn-Y-over 0.5s;
-        }
-        .filter-container{
-            animation: transitionIn-X  0.5s;
-        }
-        .sub-table{
-            animation: transitionIn-Y-bottom 0.5s;
-        }
-    </style>
-    
-    
+  <title>Settings</title>
+  <style>
+  .dashbord-tables {
+    animation: transitionIn-Y-over 0.5s;
+  }
+
+  .filter-container {
+    animation: transitionIn-X 0.5s;
+  }
+
+  .sub-table {
+    animation: transitionIn-Y-bottom 0.5s;
+  }
+  </style>
+
+
 </head>
+
 <body>
-    <?php
-
-    //learn from w3schools.com
-
+  <?php
     session_start();
 
     if(isset($_SESSION["user"])){
@@ -43,7 +43,6 @@
         header("location: ../login.php");
     }
     
-
     //import database
     include("../connection.php");
     $userrow = $database->query("select * from patient where pemail='$useremail'");
@@ -52,78 +51,100 @@
     $username=$userfetch["pname"];
 
     ?>
-    <div class="container">
-        <div class="menu">
-            <table class="menu-container" border="0">
-                <tr>
-                    <td style="padding:10px" colspan="2">
-                        <table border="0" class="profile-container">
-                            <tr>
-                                <td width="30%" style="padding-left:20px" >
-                                    <img src="../img/user.png" alt="" width="100%" style="border-radius:50%">
-                                </td>
-                                <td style="padding:0px;margin:0px;">
-                                    <p class="profile-title"><?php echo substr($username,0,13)  ?>..</p>
-                                    <p class="profile-subtitle"><?php echo substr($useremail,0,22)  ?></p>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td colspan="2">
-                                <a href="../logout.php" ><input type="button" value="Log out" class="logout-btn btn-primary-soft btn"></a>
-                                </td>
-                            </tr>
-                    </table>
-                    </td>
-                
-                </tr>
-                <tr class="menu-row" >
-                    <td class="menu-btn menu-icon-home " >
-                        <a href="index.php" class="non-style-link-menu "><div><p class="menu-text">Home</p></a></div></a>
-                    </td>
-                </tr>
-                <tr class="menu-row">
-                    <td class="menu-btn menu-icon-doctor">
-                        <a href="doctors.php" class="non-style-link-menu"><div><p class="menu-text">All Doctors</p></a></div>
-                    </td>
-                </tr>
-                
-                <tr class="menu-row" >
-                    <td class="menu-btn menu-icon-session">
-                        <a href="schedule.php" class="non-style-link-menu"><div><p class="menu-text">Scheduled Sessions</p></div></a>
-                    </td>
-                </tr>
-                <tr class="menu-row" >
-                    <td class="menu-btn menu-icon-appoinment">
-                        <a href="appointment.php" class="non-style-link-menu"><div><p class="menu-text">My Bookings</p></a></div>
-                    </td>
-                </tr>
-                <tr class="menu-row" >
-                    <td class="menu-btn menu-icon-settings  menu-active menu-icon-settings-active">
-                        <a href="settings.php" class="non-style-link-menu  non-style-link-menu-active"><div><p class="menu-text">Settings</p></a></div>
-                    </td>
-                </tr>
-                
+  <div class="container">
+    <div class="menu">
+      <table class="menu-container" border="0">
+        <tr>
+          <td style="padding:10px" colspan="2">
+            <table border="0" class="profile-container">
+              <tr>
+                <td width="30%" style="padding-left:20px">
+                  <img src="../img/user.png" alt="" width="100%" style="border-radius:50%">
+                </td>
+                <td style="padding:0px;margin:0px;">
+                  <p class="profile-title"><?php echo substr($username,0,13)  ?>..</p>
+                  <p class="profile-subtitle"><?php echo substr($useremail,0,22)  ?></p>
+                </td>
+              </tr>
+              <tr>
+                <td colspan="2">
+                  <a href="../logout.php"><input type="button" value="Log out"
+                      class="logout-btn btn-primary-soft btn"></a>
+                </td>
+              </tr>
             </table>
+          </td>
+
+        </tr>
+        <tr class="menu-row">
+          <td class="menu-btn menu-icon-home ">
+            <a href="index.php" class="non-style-link-menu ">
+              <div>
+                <p class="menu-text">Home</p>
+            </a>
+    </div></a>
+    </td>
+    </tr>
+    <tr class="menu-row">
+      <td class="menu-btn menu-icon-doctor">
+        <a href="doctors.php" class="non-style-link-menu">
+          <div>
+            <p class="menu-text">All Doctors</p>
+        </a>
+  </div>
+  </td>
+  </tr>
+
+  <tr class="menu-row">
+    <td class="menu-btn menu-icon-session">
+      <a href="schedule.php" class="non-style-link-menu">
+        <div>
+          <p class="menu-text">Scheduled Sessions</p>
         </div>
-        <div class="dash-body" style="margin-top: 15px">
-            <table border="0" width="100%" style=" border-spacing: 0;margin:0;padding:0;" >
-                        
-                        <tr >
-                            
-                        <td width="13%" >
-                    <a href="settings.php" ><button  class="login-btn btn-primary-soft btn btn-icon-back"  style="padding-top:11px;padding-bottom:11px;margin-left:20px;width:125px"><font class="tn-in-text">Back</font></button></a>
-                    </td>
-                    <td>
-                        <p style="font-size: 23px;padding-left:12px;font-weight: 600;">Settings</p>
-                                           
-                    </td>
-                    
-                            <td width="15%">
-                                <p style="font-size: 14px;color: rgb(119, 119, 119);padding: 0;margin: 0;text-align: right;">
-                                    Today's Date
-                                </p>
-                                <p class="heading-sub12" style="padding: 0;margin: 0;">
-                                    <?php 
+      </a>
+    </td>
+  </tr>
+  <tr class="menu-row">
+    <td class="menu-btn menu-icon-appoinment">
+      <a href="appointment.php" class="non-style-link-menu">
+        <div>
+          <p class="menu-text">My Bookings</p>
+      </a></div>
+    </td>
+  </tr>
+  <tr class="menu-row">
+    <td class="menu-btn menu-icon-settings  menu-active menu-icon-settings-active">
+      <a href="settings.php" class="non-style-link-menu  non-style-link-menu-active">
+        <div>
+          <p class="menu-text">Settings</p>
+      </a></div>
+    </td>
+  </tr>
+
+  </table>
+  </div>
+  <div class="dash-body" style="margin-top: 15px">
+    <table border="0" width="100%" style=" border-spacing: 0;margin:0;padding:0;">
+
+      <tr>
+
+        <td width="13%">
+          <a href="settings.php"><button class="login-btn btn-primary-soft btn btn-icon-back"
+              style="padding-top:11px;padding-bottom:11px;margin-left:20px;width:125px">
+              <font class="tn-in-text">Back</font>
+            </button></a>
+        </td>
+        <td>
+          <p style="font-size: 23px;padding-left:12px;font-weight: 600;">Settings</p>
+
+        </td>
+
+        <td width="15%">
+          <p style="font-size: 14px;color: rgb(119, 119, 119);padding: 0;margin: 0;text-align: right;">
+            Today's Date
+          </p>
+          <p class="heading-sub12" style="padding: 0;margin: 0;">
+            <?php 
                                 date_default_timezone_set('Asia/Kolkata');
         
                                 $today = date('Y-m-d');
@@ -137,104 +158,108 @@
 
 
                                 ?>
-                                </p>
-                            </td>
-                            <td width="10%">
-                                <button  class="btn-label"  style="display: flex;justify-content: center;align-items: center;"><img src="../img/calendar.svg" width="100%"></button>
-                            </td>
-        
-        
-                        </tr>
-                <tr>
-                    <td colspan="4">
-                        
-                        <center>
-                        <table class="filter-container" style="border: none;" border="0">
-                            <tr>
-                                <td colspan="4">
-                                    <p style="font-size: 20px">&nbsp;</p>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td style="width: 25%;">
-                                    <a href="?action=edit&id=<?php echo $userid ?>&error=0" class="non-style-link">
-                                    <div  class="dashboard-items setting-tabs"  style="padding:20px;margin:auto;width:95%;display: flex">
-                                        <div class="btn-icon-back dashboard-icons-setting" style="background-image: url('../img/icons/doctors-hover.svg');"></div>
-                                        <div>
-                                                <div class="h1-dashboard">
-                                                    Account Settings  &nbsp;
+          </p>
+        </td>
+        <td width="10%">
+          <button class="btn-label" style="display: flex;justify-content: center;align-items: center;"><img
+              src="../img/calendar.svg" width="100%"></button>
+        </td>
 
-                                                </div><br>
-                                                <div class="h3-dashboard" style="font-size: 15px;">
-                                                    Edit your Account Details & Change Password
-                                                </div>
-                                        </div>
-                                                
-                                    </div>
-                                    </a>
-                                </td>
-                                
-                                
-                            </tr>
-                            <tr>
-                                <td colspan="4">
-                                    <p style="font-size: 5px">&nbsp;</p>
-                                </td>
-                            </tr>
-                            <tr>
-                            <td style="width: 25%;">
-                                    <a href="?action=view&id=<?php echo $userid ?>" class="non-style-link">
-                                    <div  class="dashboard-items setting-tabs"  style="padding:20px;margin:auto;width:95%;display: flex;">
-                                        <div class="btn-icon-back dashboard-icons-setting " style="background-image: url('../img/icons/view-iceblue.svg');"></div>
-                                        <div>
-                                                <div class="h1-dashboard" >
-                                                    View Account Details
-                                                    
-                                                </div><br>
-                                                <div class="h3-dashboard"  style="font-size: 15px;">
-                                                    View Personal information About Your Account
-                                                </div>
-                                        </div>
-                                                
-                                    </div>
-                                    </a>
-                                </td>
-                                
-                            </tr>
-                            <tr>
-                                <td colspan="4">
-                                    <p style="font-size: 5px">&nbsp;</p>
-                                </td>
-                            </tr>
-                            <tr>
-                            <td style="width: 25%;">
-                                    <a href="?action=drop&id=<?php echo $userid.'&name='.$username ?>" class="non-style-link">
-                                    <div  class="dashboard-items setting-tabs"  style="padding:20px;margin:auto;width:95%;display: flex;">
-                                        <div class="btn-icon-back dashboard-icons-setting" style="background-image: url('../img/icons/patients-hover.svg');"></div>
-                                        <div>
-                                                <div class="h1-dashboard" style="color: #ff5050;">
-                                                    Delete Account
-                                                    
-                                                </div><br>
-                                                <div class="h3-dashboard"  style="font-size: 15px;">
-                                                    Will Permanently Remove your Account
-                                                </div>
-                                        </div>
-                                                
-                                    </div>
-                                    </a>
-                                </td>
-                                
-                            </tr>
-                        </table>
-                    </center>
-                    </td>
-                </tr>
-            
+
+      </tr>
+      <tr>
+        <td colspan="4">
+
+          <center>
+            <table class="filter-container" style="border: none;" border="0">
+              <tr>
+                <td colspan="4">
+                  <p style="font-size: 20px">&nbsp;</p>
+                </td>
+              </tr>
+              <tr>
+                <td style="width: 25%;">
+                  <a href="?action=edit&id=<?php echo $userid ?>&error=0" class="non-style-link">
+                    <div class="dashboard-items setting-tabs" style="padding:20px;margin:auto;width:95%;display: flex">
+                      <div class="btn-icon-back dashboard-icons-setting"
+                        style="background-image: url('../img/icons/doctors-hover.svg');"></div>
+                      <div>
+                        <div class="h1-dashboard">
+                          Account Settings &nbsp;
+
+                        </div><br>
+                        <div class="h3-dashboard" style="font-size: 15px;">
+                          Edit your Account Details & Change Password
+                        </div>
+                      </div>
+
+                    </div>
+                  </a>
+                </td>
+
+
+              </tr>
+              <tr>
+                <td colspan="4">
+                  <p style="font-size: 5px">&nbsp;</p>
+                </td>
+              </tr>
+              <tr>
+                <td style="width: 25%;">
+                  <a href="?action=view&id=<?php echo $userid ?>" class="non-style-link">
+                    <div class="dashboard-items setting-tabs" style="padding:20px;margin:auto;width:95%;display: flex;">
+                      <div class="btn-icon-back dashboard-icons-setting "
+                        style="background-image: url('../img/icons/view-iceblue.svg');"></div>
+                      <div>
+                        <div class="h1-dashboard">
+                          View Account Details
+
+                        </div><br>
+                        <div class="h3-dashboard" style="font-size: 15px;">
+                          View Personal information About Your Account
+                        </div>
+                      </div>
+
+                    </div>
+                  </a>
+                </td>
+
+              </tr>
+              <tr>
+                <td colspan="4">
+                  <p style="font-size: 5px">&nbsp;</p>
+                </td>
+              </tr>
+              <tr>
+                <td style="width: 25%;">
+                  <a href="?action=drop&id=<?php echo $userid.'&name='.$username ?>" class="non-style-link">
+                    <div class="dashboard-items setting-tabs" style="padding:20px;margin:auto;width:95%;display: flex;">
+                      <div class="btn-icon-back dashboard-icons-setting"
+                        style="background-image: url('../img/icons/patients-hover.svg');"></div>
+                      <div>
+                        <div class="h1-dashboard" style="color: #ff5050;">
+                          Delete Account
+
+                        </div><br>
+                        <div class="h3-dashboard" style="font-size: 15px;">
+                          Will Permanently Remove your Account
+                        </div>
+                      </div>
+
+                    </div>
+                  </a>
+                </td>
+
+              </tr>
             </table>
-        </div>
-    </div>
-    <?php 
+          </center>
+        </td>
+      </tr>
+
+    </table>
+  </div>
+  </div>
+  <?php 
     if($_GET){
         
         $id=$_GET["id"];
@@ -537,12 +562,11 @@
                 </div>
     ';
 
-
-
         }; }
 
     }
         ?>
 
 </body>
+
 </html>
