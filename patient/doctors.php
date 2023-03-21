0df89e4f7fccbcf156c6c39f66ed4bacf80877eb
LAPTOP-2K448739\Ishka Insaaf <ishkainsaf28@gmail.com>
2023-03-21 10:24:45 +0530

    Updated doctors.php

--- patient/doctors.php
+++ patient/doctors.php
@@ -1,27 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/animations.css">  
-    <link rel="stylesheet" href="../css/main.css">  
-    <link rel="stylesheet" href="../css/admin.css">
-        
-    <title>Doctors</title>
-    <style>
-        .popup{
-            animation: transitionIn-Y-bottom 0.5s;
-        }
-        .sub-table{
-            animation: transitionIn-Y-bottom 0.5s;
-        }
-</style>
-</head>
-<body>
-    <?php
 
-    //learn from w3schools.com
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../css/animations.css">
+  <link rel="stylesheet" href="../css/main.css">
+  <link rel="stylesheet" href="../css/admin.css">
+
+  <title>Doctors</title>
+  <style>
+  .popup {
+    animation: transitionIn-Y-bottom 0.5s;
+  }
+
+  .sub-table {
+    animation: transitionIn-Y-bottom 0.5s;
+  }
+  </style>
+</head>
+
+<body>
+  <?php
 
     session_start();
 
@@ -45,72 +46,95 @@
     $username=$userfetch["pname"];
 
     ?>
-    <div class="container">
-        <div class="menu">
-            <table class="menu-container" border="0">
-                <tr>
-                    <td style="padding:10px" colspan="2">
-                        <table border="0" class="profile-container">
-                            <tr>
-                                <td width="30%" style="padding-left:20px" >
-                                    <img src="../img/user.png" alt="" width="100%" style="border-radius:50%">
-                                </td>
-                                <td style="padding:0px;margin:0px;">
-                                    <p class="profile-title"><?php echo substr($username,0,13)  ?>..</p>
-                                    <p class="profile-subtitle"><?php echo substr($useremail,0,22)  ?></p>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td colspan="2">
-                                <a href="../logout.php" ><input type="button" value="Log out" class="logout-btn btn-primary-soft btn"></a>
-                                </td>
-                            </tr>
-                    </table>
-                    </td>
-                
-                </tr>
-                <tr class="menu-row" >
-                    <td class="menu-btn menu-icon-home " >
-                        <a href="index.php" class="non-style-link-menu "><div><p class="menu-text">Home</p></a></div></a>
-                    </td>
-                </tr>
-                <tr class="menu-row">
-                    <td class="menu-btn menu-icon-doctor menu-active menu-icon-doctor-active">
-                        <a href="doctors.php" class="non-style-link-menu non-style-link-menu-active"><div><p class="menu-text">All Doctors</p></a></div>
-                    </td>
-                </tr>
-                
-                <tr class="menu-row" >
-                    <td class="menu-btn menu-icon-session">
-                        <a href="schedule.php" class="non-style-link-menu"><div><p class="menu-text">Scheduled Sessions</p></div></a>
-                    </td>
-                </tr>
-                <tr class="menu-row" >
-                    <td class="menu-btn menu-icon-appoinment">
-                        <a href="appointment.php" class="non-style-link-menu"><div><p class="menu-text">My Bookings</p></a></div>
-                    </td>
-                </tr>
-                <tr class="menu-row" >
-                    <td class="menu-btn menu-icon-settings">
-                        <a href="settings.php" class="non-style-link-menu"><div><p class="menu-text">Settings</p></a></div>
-                    </td>
-                </tr>
-                
+  <div class="container">
+    <div class="menu">
+      <table class="menu-container" border="0">
+        <tr>
+          <td style="padding:10px" colspan="2">
+            <table border="0" class="profile-container">
+              <tr>
+                <td width="30%" style="padding-left:20px">
+                  <img src="../img/user.png" alt="" width="100%" style="border-radius:50%">
+                </td>
+                <td style="padding:0px;margin:0px;">
+                  <p class="profile-title"><?php echo substr($username,0,13)  ?>..</p>
+                  <p class="profile-subtitle"><?php echo substr($useremail,0,22)  ?></p>
+                </td>
+              </tr>
+              <tr>
+                <td colspan="2">
+                  <a href="../logout.php"><input type="button" value="Log out"
+                      class="logout-btn btn-primary-soft btn"></a>
+                </td>
+              </tr>
             </table>
-        </div>
-        <div class="dash-body">
-            <table border="0" width="100%" style=" border-spacing: 0;margin:0;padding:0;margin-top:25px; ">
-                <tr >
-                    <td width="13%">
-                        <a href="doctors.php" ><button  class="login-btn btn-primary-soft btn btn-icon-back"  style="padding-top:11px;padding-bottom:11px;margin-left:20px;width:125px"><font class="tn-in-text">Back</font></button></a>
-                    </td>
-                    <td>
-                        
-                        <form action="" method="post" class="header-search">
+          </td>
 
-                            <input type="search" name="search" class="input-text header-searchbar" placeholder="Search Doctor name or Email" list="doctors">&nbsp;&nbsp;
-                            
-                            <?php
+        </tr>
+        <tr class="menu-row">
+          <td class="menu-btn menu-icon-home ">
+            <a href="index.php" class="non-style-link-menu ">
+              <div>
+                <p class="menu-text">Home</p>
+            </a>
+    </div></a>
+    </td>
+    </tr>
+    <tr class="menu-row">
+      <td class="menu-btn menu-icon-doctor menu-active menu-icon-doctor-active">
+        <a href="doctors.php" class="non-style-link-menu non-style-link-menu-active">
+          <div>
+            <p class="menu-text">All Doctors</p>
+        </a>
+  </div>
+  </td>
+  </tr>
+
+  <tr class="menu-row">
+    <td class="menu-btn menu-icon-session">
+      <a href="schedule.php" class="non-style-link-menu">
+        <div>
+          <p class="menu-text">Scheduled Sessions</p>
+        </div>
+      </a>
+    </td>
+  </tr>
+  <tr class="menu-row">
+    <td class="menu-btn menu-icon-appoinment">
+      <a href="appointment.php" class="non-style-link-menu">
+        <div>
+          <p class="menu-text">My Bookings</p>
+      </a></div>
+    </td>
+  </tr>
+  <tr class="menu-row">
+    <td class="menu-btn menu-icon-settings">
+      <a href="settings.php" class="non-style-link-menu">
+        <div>
+          <p class="menu-text">Settings</p>
+      </a></div>
+    </td>
+  </tr>
+
+  </table>
+  </div>
+  <div class="dash-body">
+    <table border="0" width="100%" style=" border-spacing: 0;margin:0;padding:0;margin-top:25px; ">
+      <tr>
+        <td width="13%">
+          <a href="doctors.php"><button class="login-btn btn-primary-soft btn btn-icon-back"
+              style="padding-top:11px;padding-bottom:11px;margin-left:20px;width:125px">
+              <font class="tn-in-text">Back</font>
+            </button></a>
+        </td>
+        <td>
+
+          <form action="" method="post" class="header-search">
+
+            <input type="search" name="search" class="input-text header-searchbar"
+              placeholder="Search Doctor name or Email" list="doctors">&nbsp;&nbsp;
+
+            <?php
                                 echo '<datalist id="doctors">';
                                 $list11 = $database->query("select  docname,docemail from  doctor;");
 
@@ -124,41 +148,44 @@
 
                             echo ' </datalist>';
 ?>
-                            
-                       
-                            <input type="Submit" value="Search" class="login-btn btn-primary btn" style="padding-left: 25px;padding-right: 25px;padding-top: 10px;padding-bottom: 10px;">
-                        
-                        </form>
-                        
-                    </td>
-                    <td width="15%">
-                        <p style="font-size: 14px;color: rgb(119, 119, 119);padding: 0;margin: 0;text-align: right;">
-                            Today's Date
-                        </p>
-                        <p class="heading-sub12" style="padding: 0;margin: 0;">
-                            <?php 
+
+
+            <input type="Submit" value="Search" class="login-btn btn-primary btn"
+              style="padding-left: 25px;padding-right: 25px;padding-top: 10px;padding-bottom: 10px;">
+
+          </form>
+
+        </td>
+        <td width="15%">
+          <p style="font-size: 14px;color: rgb(119, 119, 119);padding: 0;margin: 0;text-align: right;">
+            Today's Date
+          </p>
+          <p class="heading-sub12" style="padding: 0;margin: 0;">
+            <?php 
                         date_default_timezone_set('Asia/Kolkata');
 
                         $date = date('Y-m-d');
                         echo $date;
                         ?>
-                        </p>
-                    </td>
-                    <td width="10%">
-                        <button  class="btn-label"  style="display: flex;justify-content: center;align-items: center;"><img src="../img/calendar.svg" width="100%"></button>
-                    </td>
+          </p>
+        </td>
+        <td width="10%">
+          <button class="btn-label" style="display: flex;justify-content: center;align-items: center;"><img
+              src="../img/calendar.svg" width="100%"></button>
+        </td>
 
 
-                </tr>
-               
-                
-                <tr>
-                    <td colspan="4" style="padding-top:10px;">
-                        <p class="heading-main12" style="margin-left: 45px;font-size:18px;color:rgb(49, 49, 49)">All Doctors (<?php echo $list11->num_rows; ?>)</p>
-                    </td>
-                    
-                </tr>
-                <?php
+      </tr>
+
+
+      <tr>
+        <td colspan="4" style="padding-top:10px;">
+          <p class="heading-main12" style="margin-left: 45px;font-size:18px;color:rgb(49, 49, 49)">All Doctors
+            (<?php echo $list11->num_rows; ?>)</p>
+        </td>
+
+      </tr>
+      <?php
                     if($_POST){
                         $keyword=$_POST["search"];
                         
@@ -168,50 +195,46 @@
 
                     }
 
-
-
                 ?>
-                  
-                <tr>
-                   <td colspan="4">
-                       <center>
-                        <div class="abc scroll">
-                        <table width="93%" class="sub-table scrolldown" border="0">
-                        <thead>
-                        <tr>
-                                <th class="table-headin">
-                                    
-                                
-                                Doctor Name
-                                
-                                </th>
-                                <th class="table-headin">
-                                    Email
-                                </th>
-                                <th class="table-headin">
-                                    
-                                    Specialties
-                                    
-                                </th>
-                                <th class="table-headin">
-                                    
-                                    Events
-                                    
-                                </tr>
-                        </thead>
-                        <tbody>
-                        
-                            <?php
 
-                                
+      <tr>
+        <td colspan="4">
+          <center>
+            <div class="abc scroll">
+              <table width="93%" class="sub-table scrolldown" border="0">
+                <thead>
+                  <tr>
+                    <th class="table-headin">
+
+
+                      Doctor Name
+
+                    </th>
+                    <th class="table-headin">
+                      Email
+                    </th>
+                    <th class="table-headin">
+
+                      Specialties
+
+                    </th>
+                    <th class="table-headin">
+
+                      Events
+
+                  </tr>
+                </thead>
+                <tbody>
+
+                  <?php
+                           
                                 $result= $database->query($sqlmain);
 
                                 if($result->num_rows==0){
                                     echo '<tr>
                                     <td colspan="4">
                                     <br><br><br><br>
-                                    <center>
-                                    <img src="../img/notfound.svg" width="25%">
+                                    <center>                     
                                     
                                     <br>
                                     <p class="heading-main12" style="margin-left: 45px;font-size:20px;color:rgb(49, 49, 49)">We  couldnt find anything related to your keywords !</p>
@@ -258,21 +281,19 @@
                             }
                                  
                             ?>
- 
-                            </tbody>
 
-                        </table>
-                        </div>
-                        </center>
-                   </td> 
-                </tr>
-                       
-                        
-                        
-            </table>
-        </div>
-    </div>
-    <?php 
+                </tbody>
+
+              </table>
+            </div>
+          </center>
+        </td>
+      </tr>
+
+    </table>
+  </div>
+  </div>
+  <?php 
     if($_GET){
         
         $id=$_GET["id"];
@@ -563,8 +584,7 @@
                                             <input type="password" name="cpassword" class="input-text" placeholder="Conform Password" required><br>
                                         </td>
                                     </tr>
-                                    
-                        
+                                                    
                                     <tr>
                                         <td colspan="2">
                                             <input type="reset" value="Reset" class="login-btn btn-primary-soft btn" >&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
@@ -607,13 +627,12 @@
                 </div>
     ';
 
-
-
         }; 
     };
 
 ?>
-</div>
+  </div>
 
 </body>
+
 </html>
