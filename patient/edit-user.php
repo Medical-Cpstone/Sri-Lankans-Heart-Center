5e2396396b73ff71642f7abca2a0a5df064a9991
LAPTOP-2K448739\Ishka Insaaf <ishkainsaf28@gmail.com>
2023-03-21 10:26:04 +0530

    Updated edit-user.php

--- patient/edit-user.php
+++ patient/edit-user.php
@@ -1,15 +1,10 @@
-
-    <?php
+<?php
     
-    
-
     //import database
     include("../connection.php");
 
-
-
     if($_POST){
-        //print_r($_POST);
+        
         $result= $database->query("select * from webuser");
         $name=$_POST['name'];
         $nic=$_POST['nic'];
@@ -25,7 +20,7 @@
             $error='3';
             $aab="select patient.pid from patient inner join webuser on patient.pemail=webuser.email where webuser.email='$email';";
             $result= $database->query($aab);
-            //$resultqq= $database->query("select * from doctor where docid='$id';");
+
             if($result->num_rows==1){
                 $id2=$result->fetch_assoc()["pid"];
             }else{
@@ -34,14 +29,10 @@
             
 
             if($id2!=$id){
-                $error='1';
-                //$resultqq1= $database->query("select * from doctor where docemail='$email';");
-                //$did= $resultqq1->fetch_assoc()["docid"];
-                //if($resultqq1->num_rows==1){
+                $error='1';         
                     
             }else{
 
-                //$sql1="insert into doctor(docemail,docname,docpassword,docnic,doctel,specialties) values('$email','$name','$password','$nic','$tele',$spec);";
                 $sql1="update patient set pemail='$email',pname='$name',ppassword='$password',pnic='$nic',ptel='$tele',paddress='$address' where pid=$id ;";
                 $database->query($sql1);
                 echo $sql1;
@@ -49,27 +40,22 @@
                 $database->query($sql1);
                 echo $sql1;
                 
-                $error= '4';
-                
+                $error= '4';              
             }
             
         }else{
             $error='2';
         }
-    
-    
-        
+      
         
     }else{
         //header('location: signup.php');
         $error='3';
     }
     
-
     header("location: settings.php?action=edit&error=".$error."&id=".$id);
     ?>
-    
-   
 
 </body>
+
 </html>
