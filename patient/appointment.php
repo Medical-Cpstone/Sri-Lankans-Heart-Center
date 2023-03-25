400c5771d7ada45edea9f942091b00fb6abcfe54
LAPTOP-2K448739\Ishka Insaaf <ishkainsaf28@gmail.com>
2023-03-25 11:47:34 +0530

    Updated appointment.php

--- patient/appointment.php
+++ patient/appointment.php
@@ -46,10 +46,6 @@
     $username=$userfetch["pname"];
 
 
-    //echo $userid;
-    //echo $username;
-
-
     $sqlmain= "select appointment.appoid,schedule.scheduleid,schedule.title,doctor.docname,patient.pname,schedule.scheduledate,schedule.scheduletime,appointment.apponum,appointment.appodate from schedule inner join appointment on schedule.scheduleid=appointment.scheduleid inner join patient on patient.pid=appointment.pid inner join doctor on schedule.docid=doctor.docid  where  patient.pid=$userid ";
 
     if($_POST){
@@ -59,8 +55,6 @@
             $sheduledate=$_POST["sheduledate"];
             $sqlmain.=" and schedule.scheduledate='$sheduledate' ";
         };
-
-        //echo $sqlmain;
 
     }
 
@@ -157,13 +151,10 @@
           </p>
           <p class="heading-sub12" style="padding: 0;margin: 0;">
             <?php 
-
-                        date_default_timezone_set('Asia/Kolkata');
-
-                        $today = date('Y-m-d');
-                        echo $today;
-
+                        date_default_timezone_set('Asia/Colombo');
                         
+                        $today = date('Y-m-d');
+                        echo $today;                       
                         ?>
           </p>
         </td>
@@ -171,17 +162,13 @@
           <button class="btn-label" style="display: flex;justify-content: center;align-items: center;"><img
               src="../img/calendar.svg" width="100%"></button>
         </td>
-
-
       </tr>
-
       <tr>
         <td colspan="4" style="padding-top:10px;width: 100%;">
 
           <p class="heading-main12" style="margin-left: 45px;font-size:18px;color:rgb(49, 49, 49)">My Bookings
             (<?php echo $result->num_rows; ?>)</p>
         </td>
-
       </tr>
       <tr>
         <td colspan="4" style="padding-top:0px;width: 100%;">
@@ -189,48 +176,33 @@
             <table class="filter-container" border="0">
               <tr>
                 <td width="10%">
-
                 </td>
                 <td width="5%" style="text-align: center;">
                   Date:
                 </td>
                 <td width="30%">
                   <form action="" method="post">
-
                     <input type="date" name="sheduledate" id="date" class="input-text filter-container-items"
                       style="margin: 0;width: 95%;">
-
                 </td>
-
                 <td width="12%">
                   <input type="submit" name="filter" value=" Filter"
                     class=" btn-primary-soft btn button-icon btn-filter" style="padding: 15px; margin :0;width:100%">
                   </form>
                 </td>
-
               </tr>
             </table>
-
           </center>
         </td>
-
       </tr>
-
-
-
       <tr>
         <td colspan="4">
           <center>
             <div class="abc scroll">
               <table width="93%" class="sub-table scrolldown" border="0" style="border:none">
-
                 <tbody>
 
-                  <?php
-
-                                
-                                
-
+                  <?php                                                     
                                 if($result->num_rows==0){
                                     echo '<tr>
                                     <td colspan="7">
@@ -276,7 +248,7 @@
                                                         <div style="width:100%;">
                                                         <div class="h3-search">
                                                                     Booking Date: '.substr($appodate,0,30).'<br>
-                                                                    Reference Number: OC-000-'.$appoid.'
+                                                                    Reference Number: RN-000-'.$appoid.'
                                                                 </div>
                                                                 <div class="h1-search">
                                                                     '.substr($title,0,21).'<br>
@@ -287,8 +259,7 @@
                                                                 <div class="h3-search">
                                                                     '.substr($docname,0,30).'
                                                                 </div>
-                                                                
-                                                                
+                                                                                                                     
                                                                 <div class="h4-search">
                                                                     Scheduled Date: '.$scheduledate.'<br>Starts: <b>@'.substr($scheduletime,0,5).'</b> (24h)
                                                                 </div>
@@ -297,8 +268,7 @@
                                                         </div>
                                                                 
                                                     </div>
-                                                </td>';
-    
+                                                </td>';   
                                         }
                                         echo "</tr>";
                                     
@@ -306,17 +276,12 @@
                             }
                                  
                             ?>
-
                 </tbody>
-
               </table>
             </div>
           </center>
         </td>
       </tr>
-
-
-
     </table>
   </div>
   </div>
@@ -332,7 +297,7 @@
                     <div class="popup">
                     <center>
                     <br><br>
-                        <h2>Booking Successfully.</h2>
+                        <h2>Booking Successfully</h2>
                         <a class="close" href="appointment.php">&times;</a>
                         <div class="content">
                         Your Appointment number is '.$id.'.<br><br>
@@ -392,20 +357,16 @@
                         <h2></h2>
                         <a class="close" href="doctors.php">&times;</a>
                         <div class="content">
-                            eDoc Web App<br>
-                            
+                                                   
                         </div>
                         <div style="display: flex;justify-content: center;">
-                        <table width="80%" class="sub-table scrolldown add-doc-form-container" border="0">
-                        
+                        <table width="80%" class="sub-table scrolldown add-doc-form-container" border="0">                      
                             <tr>
                                 <td>
                                     <p style="padding: 0;margin: 0;text-align: left;font-size: 25px;font-weight: 500;">View Details.</p><br><br>
                                 </td>
-                            </tr>
-                            
-                            <tr>
-                                
+                            </tr>                        
+                            <tr>                      
                                 <td class="label-td" colspan="2">
                                     <label for="name" class="form-label">Name: </label>
                                 </td>
@@ -413,8 +374,7 @@
                             <tr>
                                 <td class="label-td" colspan="2">
                                     '.$name.'<br><br>
-                                </td>
-                                
+                                </td>                              
                             </tr>
                             <tr>
                                 <td class="label-td" colspan="2">
@@ -460,13 +420,9 @@
                             <tr>
                                 <td colspan="2">
                                     <a href="doctors.php"><input type="button" value="OK" class="login-btn btn-primary-soft btn" ></a>
-                                
-                                    
-                                </td>
-                
-                            </tr>
-                           
-
+                                                                 
+                                </td>               
+                            </tr>                         
                         </table>
                         </div>
                     </center>
