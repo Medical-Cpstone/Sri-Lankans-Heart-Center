61ea4d9d573139924572661135c64c2d04ec3085
LAPTOP-2K448739\Ishka Insaaf <ishkainsaf28@gmail.com>
2023-04-05 13:58:28 +0530

    Update delete-appointment.php

--- doctor/delete-appointment.php
+++ doctor/delete-appointment.php
@@ -1,5 +1,4 @@
 <?php
-
     session_start();
 
     if(isset($_SESSION["user"])){
@@ -9,20 +8,14 @@
 
     }else{
         header("location: ../login.php");
-    }
-    
+    } 
     
     if($_GET){
         //import database
         include("../connection.php");
         $id=$_GET["id"];
-        //$result001= $database->query("select * from schedule where scheduleid=$id;");
-        //$email=($result001->fetch_assoc())["docemail"];
-        $sql= $database->query("delete from appointment where appoid='$id';");
-        //$sql= $database->query("delete from doctor where docemail='$email';");
-        //print_r($email);
+        $sql= $database->query("delete from appointment where appoid='$id';");    
         header("location: appointment.php");
     }
-
 
 ?>
