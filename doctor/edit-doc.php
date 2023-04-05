853ed78098d2be202974b68b0b85211cd6d85af1
LAPTOP-2K448739\Ishka Insaaf <ishkainsaf28@gmail.com>
2023-04-05 19:33:32 +0530

    Update edit-doc.php

--- doctor/edit-doc.php
+++ doctor/edit-doc.php
@@ -1,12 +1,7 @@
-
-<?php
-    
-    
+<?php 
 
     //import database
     include("../connection.php");
-
-
 
     if($_POST){
         //print_r($_POST);
@@ -24,7 +19,6 @@
         if ($password==$cpassword){
             $error='3';
             $result= $database->query("select doctor.docid from doctor inner join webuser on doctor.docemail=webuser.email where webuser.email='$email';");
-            //$resultqq= $database->query("select * from doctor where docid='$id';");
             if($result->num_rows==1){
                 $id2=$result->fetch_assoc()["docid"];
             }else{
@@ -34,42 +28,26 @@
             echo $id2."jdfjdfdh";
             if($id2!=$id){
                 $error='1';
-                //$resultqq1= $database->query("select * from doctor where docemail='$email';");
-                //$did= $resultqq1->fetch_assoc()["docid"];
-                //if($resultqq1->num_rows==1){
-                    
             }else{
-
-                //$sql1="insert into doctor(docemail,docname,docpassword,docnic,doctel,specialties) values('$email','$name','$password','$nic','$tele',$spec);";
                 $sql1="update doctor set docemail='$email',docname='$name',docpassword='$password',docnic='$nic',doctel='$tele',specialties=$spec where docid=$id ;";
                 $database->query($sql1);
-
                 $sql1="update webuser set email='$email' where email='$oldemail' ;";
                 $database->query($sql1);
-
                 echo $sql1;
                 //echo $sql2;
-                $error= '4';
-                
+                $error= '4';             
             }
             
         }else{
             $error='2';
-        }
-    
-    
-        
+        }       
         
     }else{
         //header('location: signup.php');
         $error='3';
     }
-    
-
     header("location: settings.php?action=edit&error=".$error."&id=".$id);
     ?>
-    
-   
-
 </body>
+
 </html>
