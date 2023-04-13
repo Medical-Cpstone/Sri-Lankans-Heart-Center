369510ac7022da92088139ef5dea6adfa5ae27e4
LAPTOP-2K448739\Ishka Insaaf <ishkainsaf28@gmail.com>
2023-04-13 13:03:24 +0530

    Update settings.php

--- patient/settings.php
+++ patient/settings.php
@@ -60,7 +60,7 @@
                   <img src="../img/user.png" alt="" width="100%" style="border-radius:50%">
                 </td>
                 <td style="padding:0px;margin:0px;">
-                  <p class="profile-title"><?php echo substr($username,0,13)  ?>..</p>
+                  <p class="profile-title"><?php echo substr($username,0,13)  ?></p>
                   <p class="profile-subtitle"><?php echo substr($useremail,0,22)  ?></p>
                 </td>
               </tr>
@@ -143,7 +143,7 @@
           </p>
           <p class="heading-sub12" style="padding: 0;margin: 0;">
             <?php 
-                                date_default_timezone_set('Asia/Kolkata');
+                                date_default_timezone_set('Asia/Colombo');
         
                                 $today = date('Y-m-d');
                                 echo $today;
