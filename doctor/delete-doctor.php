685b556d39277d17bca53d500b84fceb702843e5
DESKTOP-88BE66H\hsnhs <s92069432@ousl.lk>
2023-12-06 15:04:29 +0530

    Update changes

---
+++ doctor/delete-doctor.php
@@ -0,0 +1,37 @@
+<?php
+
+    session_start();
+
+    if(isset($_SESSION["user"])){
+        if(($_SESSION["user"])=="" or $_SESSION['usertype']!='d'){
+            header("location: ../login.php");
+        }else{
+            $useremail=$_SESSION["user"];
+        }
+
+    }else{
+        header("location: ../login.php");
+    }
+    
+
+    //import database
+    include("../connection.php");
+    $userrow = $database->query("select * from doctor where docemail='$useremail'");
+    $userfetch=$userrow->fetch_assoc();
+    $userid= $userfetch["docid"];
+    $username=$userfetch["docname"];
+
+    
+    if($_GET){
+        include("../connection.php");
+        $id=$_GET["id"];
+        $result001= $database->query("select * from doctor where docid=$id;");
+        $email=($result001->fetch_assoc())["docemail"];
+        $sql= $database->query("delete from webuser where email='$email';");
+        $sql= $database->query("delete from doctor where docemail='$email';");
+        //print_r($email);
+        header("location: ../logout.php");
+}
+
+
+?>
